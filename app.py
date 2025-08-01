3db83cec030ff178be6bada784a73eec3dc0b5eb
soto00947 <sonammtobdenn@gmail.com>
2025-08-01 14:11:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from supabase import create_client
 from datetime import datetime
 
 # Supabase credentials
-url = "https://your-project-id.supabase.co"
-key = "your-anon-key"
+url = "https://wpitatsigfjhcaqwkqvo.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwaXRhdHNpZ2ZqaGNhcXdrcXZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwNDYzNjEsImV4cCI6MjA2OTYyMjM2MX0.8vg70wT9HhXa3XlBMnc68Jii8-qUYrz7q8khmUxlsKo"
 supabase = create_client(url, key)
 
 st.set_page_config(page_title="Toilet Status", page_icon="🚽")
