64fddf27e5b53e8c56be7ce5935ad128ae793ad5
soto00947 <sonammtobdenn@gmail.com>
2025-08-04 12:14:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,6 @@ key = st.secrets["SUPABASE_KEY"]
 
 supabase = create_client(url, key)
 
-# Supabase credentials
-#url = "https://wpitatsigfjhcaqwkqvo.supabase.co"
-#key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwaXRhdHNpZ2ZqaGNhcXdrcXZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwNDYzNjEsImV4cCI6MjA2OTYyMjM2MX0.8vg70wT9HhXa3XlBMnc68Jii8-qUYrz7q8khmUxlsKo"
 supabase = create_client(url, key)
 
 st.set_page_config(page_title="Toilettenstatus", page_icon="🚽")
