c146a7e1b3f91a9c7302891c3efc5e26763ba232
soto00947 <sonammtobdenn@gmail.com>
2025-08-04 11:59:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ from dotenv import load_dotenv
 import time
 
 # 🔐 Load environment variables
-load_dotenv()
+'''load_dotenv()
 url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
+key = os.getenv("SUPABASE_KEY")'''
+
+url="https://wpitatsigfjhcaqwkqvo.supabase.co"
+key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwaXRhdHNpZ2ZqaGNhcXdrcXZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwNDYzNjEsImV4cCI6MjA2OTYyMjM2MX0.8vg70wT9HhXa3XlBMnc68Jii8-qUYrz7q8khmUxlsKo"
 supabase = create_client(url, key)
 
 st.set_page_config(page_title="Toilettenstatus", page_icon="🚽")
